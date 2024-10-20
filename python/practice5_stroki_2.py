718e9ed6172760768f4293a3f4de05899b9a42bf
5tuffy13 <sashashem25052006@gmail.com>
2024-10-20 10:22:15 +0800

    Create practice5_stroki_2.py

---
+++ python/practice5_stroki_2.py
@@ -0,0 +1,9 @@
+n=[int(i) for i in input().split(" ")]
+s=input()
+words=[]
+for i in range(n[0]-n[1]-1):
+    w=s[i:n[1]+i]
+    print(w)
+    if w not in words:
+        words.append(w)
+print(len(words),words)
