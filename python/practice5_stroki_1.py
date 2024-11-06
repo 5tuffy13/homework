709fa40515e29f73774204d14d4c2fcfa95b0a41
5tuffy13 <sashashem25052006@gmail.com>
2024-11-06 21:24:22 +0800

    Update practice5_stroki_1.py

--- python/practice5_stroki_1.py
+++ python/practice5_stroki_1.py
@@ -1,18 +1,20 @@
+def f(s):
+    if s[4]=='x' and s[1]=='+':
+        return (int(s[0])+int(s[2]))
+    
+    elif s[4]=='x' and s[1]=='-':
+        return (int(s[0])-int(s[2]))
+    
+    elif s[0]=='x' and s[1]=='+':
+        return (int(s[4])-int(s[2]))
+    
+    elif s[0]=='x' and s[1]=='-':
+        return (int(s[2])+int(s[4]))
+    
+    elif s[2]=='x' and s[1]=='+':
+        return (int(s[4])-int(s[0]))
+    else:
+        return (int(s[0])-int(s[4]))
+
 s=input()
-if s[4]=='x' and s[1]=='+':
-    print(int(s[0])+int(s[2]))
-    
-elif s[4]=='x' and s[1]=='-':
-    print(int(s[0])-int(s[2]))
-    
-elif s[0]=='x' and s[1]=='+':
-    print(int(s[4])-int(s[2]))
-    
-elif s[0]=='x' and s[1]=='-':
-    print(int(s[2])+int(s[4]))
-    
-elif s[2]=='x' and s[1]=='+':
-    print(int(s[4])-int(s[0]))
-else:
-    print(int(s[0])-int(s[4]))
-    
+print(f(s))
